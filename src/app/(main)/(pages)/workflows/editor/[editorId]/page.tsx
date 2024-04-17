80e592659c55981b6c6dd465f279672e1fe5f7a0
ahoang2502 <athoang252@gmail.com>
2024-04-17 18:24:21 +1000

    feat: editorId page: connections provider

--- src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 import { EditorProvider } from "@/providers/EditorProvider";
+import { ConnectionsProvider } from "@/providers/ConnectionsProvider";
 
 const EditorIdPage = () => {
-  return <div className="h-full">
-    <EditorProvider>
-
-    </EditorProvider>
-  </div>;
+  return (
+    <div className="h-full">
+      <EditorProvider>
+        <ConnectionsProvider>
+            <div className=""></div>
+        </ConnectionsProvider>
+      </EditorProvider>
+    </div>
+  );
 };
 
 export default EditorIdPage;
