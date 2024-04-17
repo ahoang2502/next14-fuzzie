0b55c88126c57a00d42ecbd0836f59fc8375e11a
ahoang2502 <athoang252@gmail.com>
2024-04-17 18:10:55 +1000

    feat: editorId page: editor reducers

---
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditorIdPage = () => {
+  return <div className="h-full">
+    <EditorProvider>
+        
+    </EditorProvider>
+  </div>;
+};
+
+export default EditorIdPage;
--- src/app/(main)/(pages)/workflows/editor/page.tsx
+++ src/app/(main)/(pages)/workflows/editor/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const EditorPage = () => {
+  // WIP redirect to editorId or prompt to create a workflow or send back to workflow page
   return <div>EditorPage</div>;
 };
 
