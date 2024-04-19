0f653df925d85a18e7f23ab70ebf1a47d5379ac9
ahoang2502 <athoang252@gmail.com>
2024-04-19 21:26:34 +1000

    feat: editorId page: connection accordion

--- src/app/(main)/(pages)/workflows/editor/[editorId]/_components/RenderConnectionAccordion.tsx
+++ src/app/(main)/(pages)/workflows/editor/[editorId]/_components/RenderConnectionAccordion.tsx
@@ -5,6 +5,10 @@ import React from "react";
 import { Connection } from "@/lib/types";
 import { EditorState } from "@/providers/EditorProvider";
 import { useNodeConnections } from "@/providers/ConnectionsProvider";
+import { useFuzzieStore } from "@/store";
+import { AccordionContent } from "@/components/ui/accordion";
+import { ConnectionCard } from "@/app/(main)/(pages)/connections/_components/ConnectionCard";
+import { MultipleSelector } from "@/components/ui/multiple-selector";
 
 type Props = {
   state: EditorState;
@@ -23,6 +27,55 @@ export const RenderConnectionAccordion = ({ state, connection }: Props) => {
   } = connection;
 
   const { nodeConnection } = useNodeConnections();
+  const { slackChannels, selectedSlackChannels, setSelectedSlackChannels } =
+    useFuzzieStore();
 
-  return <div></div>;
+  const connectionData = (nodeConnection as any)[connectionKey];
+  const isConnected =
+    alwaysTrue ||
+    (nodeConnection[connectionKey] &&
+      accessTokenKey &&
+      connectionData[accessTokenKey]);
+
+  return (
+    <AccordionContent key={title}>
+      {state.editor.selectedNode.data.title === title && (
+        <>
+          <ConnectionCard
+            title={title}
+            icon={image}
+            description={description}
+            type={title}
+            connected={{ [title]: isConnected }}
+          />
+
+          {slackSpecial && isConnected && (
+            <div className="p-6">
+              {slackChannels?.length ? (
+                <>
+                  <div className="mb-4 ml-1">
+                    Select the slack channels to send notification and messages:
+                  </div>
+
+                  <MultipleSelector
+                    value={selectedSlackChannels}
+                    onChange={setSelectedSlackChannels}
+                    defaultOptions={slackChannels}
+                    placeholder="Select channels"
+                    emptyIndicator={
+                      <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                        no results found.
+                      </p>
+                    }
+                  />
+                </>
+              ) : (
+                "No Slack channels found. Please add your Slack bot to your Slack channel"
+              )}
+            </div>
+          )}
+        </>
+      )}
+    </AccordionContent>
+  );
 };
