0a9711c08a3c7120e075ba7cbe09c57e02844a7c
ahoang2502 <athoang252@gmail.com>
2024-04-22 21:44:35 +1000

    feat: connections page: slack, discord, notion

--- src/app/(main)/(pages)/connections/_actions/notion-connection.ts
+++ src/app/(main)/(pages)/connections/_actions/notion-connection.ts
@@ -1,7 +1,9 @@
-'use server'
+"use server";
 
 import { currentUser } from "@clerk/nextjs";
 import { Client } from "@notionhq/client";
+
+import { db } from "@/lib/db";
 
 export const onCreateNewPageInDatabase = async (
   databaseId: string,
@@ -30,5 +32,51 @@ export const onCreateNewPageInDatabase = async (
   });
   if (response) {
     return response;
+  }
+};
+
+export const onNotionConnect = async (
+  access_token: string,
+  workspace_id: string,
+  workspace_icon: string,
+  workspace_name: string,
+  database_id: string,
+  id: string
+) => {
+  "use server";
+  if (access_token) {
+    //check if notion is connected
+    const notion_connected = await db.notion.findFirst({
+      where: {
+        accessToken: access_token,
+      },
+      include: {
+        connections: {
+          select: {
+            type: true,
+          },
+        },
+      },
+    });
+
+    if (!notion_connected) {
+      //create connection
+      await db.notion.create({
+        data: {
+          userId: id,
+          workspaceIcon: workspace_icon!,
+          accessToken: access_token,
+          workspaceId: workspace_id!,
+          workspaceName: workspace_name!,
+          databaseId: database_id,
+          connections: {
+            create: {
+              userId: id,
+              type: "Notion",
+            },
+          },
+        },
+      });
+    }
   }
 };
--- src/app/(main)/(pages)/connections/_actions/slack-connection.ts
+++ src/app/(main)/(pages)/connections/_actions/slack-connection.ts
@@ -3,6 +3,7 @@
 import axios from "axios";
 
 import { Option } from "@/store";
+import { db } from "@/lib/db";
 
 const postMessageInSlackChannel = async (
   slackAccessToken: string,
@@ -50,4 +51,40 @@ export const postMessageToSlack = async (
   }
 
   return { message: "Success" };
+};
+
+export const onSlackConnect = async (
+  app_id: string,
+  authed_user_id: string,
+  authed_user_token: string,
+  slack_access_token: string,
+  bot_user_id: string,
+  team_id: string,
+  team_name: string,
+  user_id: string
+): Promise<void> => {
+  if (!slack_access_token) return;
+
+  const slackConnection = await db.slack.findFirst({
+    where: { slackAccessToken: slack_access_token },
+    include: { connections: true },
+  });
+
+  if (!slackConnection) {
+    await db.slack.create({
+      data: {
+        userId: user_id,
+        appId: app_id,
+        authedUserId: authed_user_id,
+        authedUserToken: authed_user_token,
+        slackAccessToken: slack_access_token,
+        botUserId: bot_user_id,
+        teamId: team_id,
+        teamName: team_name,
+        connections: {
+          create: { userId: user_id, type: "Slack" },
+        },
+      },
+    });
+  }
 };
